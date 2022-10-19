e8d7dda1b99491151af726ca3847c3ab064f2f9a
liz435 <liz435@newschool.edu>
2022-10-19 16:41:52 -0400

    lil js

--- js/site.js
+++ js/site.js
@@ -1,112 +1,32 @@
-const useriD = "Z";
-const psd = "0124";
-const verson = "v1"
-const run = true;
-var psdIn = "";
-var idIn = "";
-var sayWhat = ""
-var sayStay = [];
-var saySome = true;
-var done = "";
+const box = document.createElement('div');
+box.classList.add("box");
+document.body.appendChild(box);
 
+var randomGenerate ={
+    id:true
+};
 
-const createId = function(newId, newPsd){
-    var savedId = newId;
-    var savedPsd = newPsd;
-    console.log(" Hi!" + savedId);
-    logged();
+let info;
 
+fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2022-09-07&end_date=2022-09-08&api_key=kEUozmDTguvnQPEtSsYX6J0zXWJjQ5tKapgRwbct')
+.then(res =>res.json())
+.then(data => collectData(data))
+
+function collectData(data){
+const listAll = data;
+for(let i=0; i<data.near_earth_objects.length; i++){
+    console.log(data.near_earth_objects[i]);
 }
+console.log(data);
+console.log(data.near_earth_objects[0]);
 
-const login = function(idIn, psdIn){
+  const countElement = document.createElement("h1");
+  countElement.innerHTML = data.element_count;
+  document.body.appendChild(countElement);
 
-    if(idIn!=useriD){
-        console.log("wrong ID");
-        console.log("you can create username and ID createId(newId, newPsd)");
-        
-    }else if(psdIn!=psd){
-        console.log("wrong password");
-        console.log("you can create username and ID createId(newId, newPsd)");
-    }else{
-        var log={
-            stored:[],
-            storedId:[],
-            status: false,
-            store:function(){
-                var nowtime = currentDate.getDate()
-                stored.push(this.nowTime);
-                return this.stored;
-            }
-        }
-        logged(idIn);
-
-    }
-}
-
-function logged(idIn){
-    console.log("you logged in!");
-    var access = {
-            computer:"old",
-            disk:"full",
-            oven:"hot",
-            milk: true,
-            red:true,
-            }
-    function next(idIn){
-        for( i=0;i<Math.random()*24;i++){
-            let output = [];
-            console.log("idIn");
-            const thisV = 3;
-
-        }
-    }
-    
-}
-
-const book ={
-    name:"Byme",
-    author:"By me",
-    yes: "yes",
-    read:function(){
-        console.log("i can't read");
-        console.log("more and more");
-        const more = function(){
-            console.log("i can't")
-        yes = "no";
-        return yes
-        }
-    },
-    moreandmore:"please",
-        
-}
-
-const setting ={
-    mode:"normal",
-    base:"e",
-    people:[],
-    true: true,
-    check:function(){
-        if(check === true){
-            var saved={
-                checkStat:this.true,
-            }
-            return "checked"
-        }
-    },
-    modeChange:function(modeSet){
-        mode = modSet;
-        return mode
-    },
-    baseChange:function(baseSet){
-        base = baseSet;
-        return base
-    },
-    addPeople:function(addPpl){
-        people = addPpl;
-        return base
-    },
-
-    
-        
+  let date = "2022-09-07";
+  const obj_id = document.createElement("h1");
+  obj_id.innerHTML = data.near_earth_objects[0][0];
+  document.body.appendChild(obj_id);
 }
 
